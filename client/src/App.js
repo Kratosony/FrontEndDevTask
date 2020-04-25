3d98c7f8b4f3acbcf69c805c03c008f23b2eef74
CRIMSONWING\warren.abdilla <Warren.Abdilla@kpmg.co.uk>
2020-04-25 22:46:39 +0200

    Main React App created with SASS

---
+++ client/src/App.js
@@ -0,0 +1,11 @@
+import React, {Component} from 'react';
+import Home from 'Components/Home';
+import './App.scss';
+
+class App extends Component {
+    render() {
+        return ( <Home/> );
+    }
+}
+
+export default App;
